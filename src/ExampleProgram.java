17a7a8e4d1531e3f1a733e097d9debe13c592bee
saravanan <saravanan@gmail.com>
2017-08-14 20:32:52 -0700

    vlcmedia-commit

---
+++ src/ExampleProgram.java
@@ -0,0 +1,9 @@
+class ExampleProgram {
+  public static void main(String[] args){
+    System.out.println("Simple Java source code to demonstrate\ncommandline or manual compilation process");
+  }
+  public void TalkToRobo()
+  {
+    System.out.println("Hello");
+  }
+}
